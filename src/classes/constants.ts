c8df9cf4322967938667c96cf3ee7bcdc27b2285
0xodia <0xodia@solend.fi>
2022-02-25 10:40:08 -0800

    Fix ctoken minting

--- src/classes/constants.ts
+++ src/classes/constants.ts
@@ -1,3 +1,4 @@
 export const WAD = "1".concat(Array(18 + 1).join("0"));
 export const WANG = "1".concat(Array(36 + 1).join("0"));
 export const U64_MAX = "18446744073709551615";
+export const SLOTS_PER_YEAR = 63072000;
