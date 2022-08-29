3afa290c1c6765150f3857805525a56e61c14cc7
0xripleys <0xripleys@solend.fi>
2022-08-29 17:27:16 -0400

    update sdk to use v1/markets/config and add break flash loans script

--- src/classes/constants.ts
+++ src/classes/constants.ts
@@ -5,5 +5,6 @@ export const WANG = "1".concat(Array(36 + 1).join("0"));
 export const U64_MAX = "18446744073709551615";
 export const SLOTS_PER_YEAR = 63072000;
 
+export const SOLEND_PRODUCTION_PROGRAM_ID = new PublicKey("So1endDq2YkqhipRh3WViPa8hdiSpxWy6z3Z6tMCpAo")
 export const SOLEND_DEVNET_PROGRAM_ID = new PublicKey("ALend7Ketfx5bxh6ghsCDXAoDrhvEmsXT3cynB6aPLgx");
 export const SOLEND_BETA_PROGRAM_ID = new PublicKey("BLendhFh4HGnycEDDFhbeFEUYLP4fXB5tTHMoTX8Dch5");
