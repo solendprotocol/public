8c0f84155312d6c3a8a719c93c99b0336ee68869
Tahsin Tunan <tahsintunan@gmail.com>
2022-11-07 03:45:14 +0600

    usePoolsList hook implemented to fetch pool vm
    Signed-off-by: Tahsin Tunan <tahsintunan@gmail.com>

---
+++ src/common/config.ts
@@ -0,0 +1,6 @@
+import { Connection } from "@solana/web3.js";
+
+
+export const ENVIRONMENT = process.env.NEXT_PUBLIC_REACT_APP_NETWORK as string || "production";
+export const RPC_ENDPOINT = process.env.NEXT_PUBLIC_REACT_APP_SOLANA_RPC_HOST as string;
+export const CONNECTION = new Connection(RPC_ENDPOINT, "confirmed");
--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,69 +1,49 @@
-import { FC, useState } from "react";
-import Link from "next/link";
+import { useState } from "react";
 import { Hambuger } from "../components";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { ReactSVG } from "react-svg";
 import { useAtom } from "jotai";
 import { themeAtom } from "stores/themeStore";
+import Image from "next/image";
+import { usePoolsList } from "hooks/usePoolsList";
 
 const AppBar = ({}) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [selectedPool, setSelectedPool] = useState("Main pool");
-
-  const pools = [
-    { name: "Main pool" },
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-  ];
+  const [selectedPool, setSelectedPool] = useState("main");
   const [theme, setTheme] = useAtom(themeAtom);
+  const { poolList, isLoading, isError } = usePoolsList();
 
+  // TODO: return proper loading and error here
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error!</div>;
+
+  const poolListItems = poolList!.map((p) => (
+    <li key={p.address} className={`${selectedPool == p.name && "bordered"}`}>
+      <a className="text-xlg">{p.name ? p.name : p.address}</a>
+    </li>
+  ));
+
+  // TODO: Merge this with Drawer.tsx (so that we have only one component for the sidebar)
   return (
     <div className="bg-neutral w-full md:w-60 inset-y-0">
+      
       {/* desktop side bar */}
       <aside className="md:w-60 md:flex lg:flex inset-y-0 h-full hidden flex-col border-r-2 border-base-200 gap-6 fixed">
         <span className="flex items-center gap-2 px-6 pt-10">
-          <img src="/Group.png" className="w-8" />
-
+          <Image
+            src="/Group.png"
+            alt="Solend Logo"
+            width={25}
+            height={25}
+            className="w-8"
+          />
           <h1 className="text-xl bold md:text-2xl lg:text-2xl">Solend Lite</h1>
         </span>
         <span className="px-4">
           <WalletMultiButton className="btn btn-primary w-full" />
         </span>
         <div className="h-4/6  flex flex-col">
-          <ul className="menu overflow-y-scroll ">
-            {pools.map((pool) => (
-              <li className={`${selectedPool == pool?.name && "bordered"}`}>
-                <a className="text-xlg">{pool?.name || "Pool"}</a>
-              </li>
-            ))}
-          </ul>
+          <ul className="menu overflow-y-scroll ">{poolListItems}</ul>
         </div>
 
         {/* desktop footer */}
@@ -81,13 +61,20 @@ const AppBar = ({}) => {
           </span>
         </div>
       </aside>
+
       {/* mobile nav bar */}
       <nav className="bg-neutral flex flex-row  w-full p-2 py-4 gap-2 items-center lg:hidden md:hidden">
         <label htmlFor="my-drawer" className="">
           <Hambuger />
         </label>
         <span className="flex items-center gap-2">
-          <img src="/Group.png" className="w-8" />
+          <Image
+            src="/Group.png"
+            alt="Solend Logo"
+            width={25}
+            height={25}
+            className="w-8"
+          />
 
           <h1 className="text-2xl bold">Solend Lite</h1>
         </span>
