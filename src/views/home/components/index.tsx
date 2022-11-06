5d1972459eaf151f43296b568a5170b37c48c36a
Janvinsha <mac@Janvinsha-2.local>
2022-11-06 19:21:52 +0100

    feat:pool position modal

--- src/views/home/components/index.tsx
+++ src/views/home/components/index.tsx
@@ -3,3 +3,4 @@ export { default as Borrow } from "./Borrow";
 export { default as Repay } from "./Repay";
 export { default as Supply } from "./Supply";
 export { default as Withdraw } from "./Withdraw";
+export { default as PoolPositionModal } from "./PoolPositionModal";
