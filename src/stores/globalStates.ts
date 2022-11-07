2812fe8d8aeaf0c821b254968fbdb2ab807a6f63
Tahsin Tunan <tahsintunan@gmail.com>
2022-11-07 20:40:44 +0600

    made selectedPool a global state atom
    Signed-off-by: Tahsin Tunan <tahsintunan@gmail.com>

---
+++ src/stores/globalStates.ts
@@ -0,0 +1,10 @@
+import { atom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
+
+
+export const themeAtom = atomWithStorage("theme", "dark");
+
+export const selectedPoolAtom = atom({
+    address: "4UpD2fh7xH3VP9QQaXtsS1YY3bxzWhtfpks7FatyKvdY",
+    name: "main",
+});
