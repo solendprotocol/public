edad114d24a5b70320049bdac42ed387f5dd9300
dasichuan <0xdasichuan@gmail.com>
2022-11-22 09:10:07 -0700

    Add test for interface fulfillment

---
+++ solend-sdk/__tests__/rpc.test.ts
@@ -0,0 +1,28 @@
+import { Connection } from '@solana/web3.js';
+import { StatsD } from 'hot-shots';
+import { InstrumentedConnection, MultiConnection, RetryConnection, SolendRPCConnection } from '../src/utils/rpc';
+
+jest.setTimeout(1_000);
+
+describe("calculate", function () {
+  it("checks interface fulfillment", async function () {
+    const connection = new Connection('https://api.mainnet-beta.solana.com', {
+      commitment: "finalized",
+    });
+    const multiConnection = new MultiConnection([connection]);
+    _checkInterface(multiConnection);
+
+    const retryConection = new RetryConnection(connection, 10);
+    _checkInterface(retryConection);
+
+    const stats = new StatsD({mock: true});
+    const instrumentedConnection = new InstrumentedConnection(connection, stats, '');
+    _checkInterface(instrumentedConnection);
+  });
+});
+
+// You can't check if a class implements an interface at runtime, so we can 
+// just do it at compile time here with a function that does nothing.
+function _checkInterface(connection: SolendRPCConnection) {
+  return;
+}
---
+++ solend-sdk/src/utils/rpc/retry.ts
@@ -0,0 +1,165 @@
+import {
+  AccountInfo,
+  AddressLookupTableAccount,
+  Blockhash,
+  BlockhashWithExpiryBlockHeight,
+  Commitment,
+  ConfirmedSignatureInfo,
+  ConfirmedSignaturesForAddress2Options,
+  FeeCalculator,
+  Finality,
+  GetAccountInfoConfig,
+  GetLatestBlockhashConfig,
+  GetMultipleAccountsConfig,
+  GetProgramAccountsConfig,
+  GetSlotConfig,
+  GetTransactionConfig,
+  GetVersionedTransactionConfig,
+  PublicKey,
+  RpcResponseAndContext,
+  SendOptions,
+  SimulatedTransactionResponse,
+  SimulateTransactionConfig,
+  TokenAmount,
+  TransactionResponse,
+  TransactionSignature,
+  VersionedTransaction,
+  VersionedTransactionResponse,
+} from "@solana/web3.js";
+import { SolendRPCConnection } from "./interface";
+
+// Adds retries to RPC Calls
+export class RetryConnection {
+  connection: SolendRPCConnection;
+  maxRetries: number;
+  public rpcEndpoint: string;
+  constructor(connection: SolendRPCConnection, maxRetries: number = 3) {
+    this.connection = connection;
+    this.maxRetries = maxRetries;
+    this.rpcEndpoint = this.connection.rpcEndpoint;
+  }
+
+  async getAccountInfo(
+    publicKey: PublicKey,
+    commitmentOrConfig?: Commitment | GetAccountInfoConfig
+  ): Promise<AccountInfo<Buffer> | null> {
+    return this.withRetries(
+      this.connection.getAccountInfo(publicKey, commitmentOrConfig)
+    );
+  }
+  async getConfirmedSignaturesForAddress2(
+    address: PublicKey,
+    options?: ConfirmedSignaturesForAddress2Options,
+    commitment?: Finality
+  ): Promise<Array<ConfirmedSignatureInfo>> {
+    return this.withRetries(
+      this.connection.getConfirmedSignaturesForAddress2(
+        address,
+        options,
+        commitment
+      )
+    );
+  }
+  getLatestBlockhash(
+    commitmentOrConfig?: Commitment | GetLatestBlockhashConfig
+  ): Promise<BlockhashWithExpiryBlockHeight> {
+    return this.withRetries(
+      this.connection.getLatestBlockhash(commitmentOrConfig)
+    );
+  }
+  getMultipleAccountsInfo(
+    publicKeys: PublicKey[],
+    commitmentOrConfig?: Commitment | GetMultipleAccountsConfig
+  ): Promise<(AccountInfo<Buffer> | null)[]> {
+    return this.withRetries(
+      this.connection.getMultipleAccountsInfo(publicKeys, commitmentOrConfig)
+    );
+  }
+  getProgramAccounts(
+    programId: PublicKey,
+    configOrCommitment?: GetProgramAccountsConfig | Commitment
+  ): Promise<
+    Array<{
+      pubkey: PublicKey;
+      account: AccountInfo<Buffer>;
+    }>
+  > {
+    return this.withRetries(
+      this.connection.getProgramAccounts(programId, configOrCommitment)
+    );
+  }
+  getRecentBlockhash(commitment?: Commitment): Promise<{
+    blockhash: Blockhash;
+    feeCalculator: FeeCalculator;
+  }> {
+    return this.withRetries(this.connection.getRecentBlockhash(commitment));
+  }
+  getSlot(commitmentOrConfig?: Commitment | GetSlotConfig): Promise<number> {
+    return this.withRetries(this.connection.getSlot(commitmentOrConfig));
+  }
+  getTokenAccountBalance(
+    tokenAddress: PublicKey,
+    commitment?: Commitment
+  ): Promise<RpcResponseAndContext<TokenAmount>> {
+    return this.withRetries(
+      this.connection.getTokenAccountBalance(tokenAddress, commitment)
+    );
+  }
+  getTokenSupply(
+    tokenMintAddress: PublicKey,
+    commitment?: Commitment
+  ): Promise<RpcResponseAndContext<TokenAmount>> {
+    return this.withRetries(
+      this.connection.getTokenSupply(tokenMintAddress, commitment)
+    );
+  }
+  getTransaction(
+    signature: string,
+    rawConfig?: GetTransactionConfig
+  ): Promise<TransactionResponse | null>;
+  getTransaction(
+    signature: string,
+    rawConfig: GetVersionedTransactionConfig
+  ): Promise<VersionedTransactionResponse | null> {
+    return this.withRetries(
+      this.connection.getTransaction(signature, rawConfig)
+    );
+  }
+  sendTransaction(
+    transaction: VersionedTransaction,
+    options?: SendOptions
+  ): Promise<TransactionSignature> {
+    return this.withRetries(
+      this.connection.sendTransaction(transaction, options)
+    );
+  }
+  simulateTransaction(
+    transaction: VersionedTransaction,
+    config?: SimulateTransactionConfig
+  ): Promise<RpcResponseAndContext<SimulatedTransactionResponse>> {
+    return this.withRetries(
+      this.connection.simulateTransaction(transaction, config)
+    );
+  }
+  getAddressLookupTable(
+    accountKey: PublicKey,
+    config?: GetAccountInfoConfig
+  ): Promise<RpcResponseAndContext<AddressLookupTableAccount | null>> {
+    return this.withRetries(
+      this.connection.getAddressLookupTable(accountKey, config)
+    );
+  }
+  async withRetries(fn: Promise<any>) {
+    let numTries = 0;
+    let lastException;
+    while (numTries <= this.maxRetries) {
+      try {
+        return await fn;
+      } catch (e: any) {
+        lastException = e;
+        numTries += 1;
+      }
+    }
+    throw lastException;
+  }
+}
