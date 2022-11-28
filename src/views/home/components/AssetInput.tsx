29e1916a07ba2a7eb4200d2dc6f2ef312602b4ef
Janvinsha <mac@jvs.local>
2022-11-28 19:46:24 +0100

    fix: hambuger revert when clicked on pools

---
+++ src/views/home/components/AssetInput.tsx
@@ -0,0 +1,8 @@
+import { useState, useRef, useEffect } from "react";
+import Link from "next/link";
+import { ReactSVG } from "react-svg";
+
+const AssetInput = () => {
+  return <div className="flex flex-row w-full py-6 justify-between"></div>;
+};
+export default AssetInput;
