50a33e4f539dd022efbe920084c121d8fa31ef08
0xodia <0xodia@solend.fi>
2023-02-10 01:37:02 -0800

    remove solend-os

--- solend-os/components/footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import { Flex } from "@chakra-ui/react";
-
-export function Footer() {
-    return <Flex>
-            Footer
-    </Flex>
-}
--- solend-os/stores/wallet.ts
+++
@@ -1,6 +0,0 @@
-import { PublicKey } from "@solana/web3.js";
-import { atom } from "jotai";
-
-export const publicKeyAtom = atom<PublicKey | null>(
-    null
-);
